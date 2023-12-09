acd7307513be98d7287d2e274dc1e9158855fe5f
svetdio <122599561+svetdio@users.noreply.github.com>
2023-12-10 00:23:10 +0800

    implemented viewable keywords and disabled save button if text area is empty

--- datafeeder/js/script.js
+++ datafeeder/js/script.js
@@ -1,36 +1,43 @@
 $(function () {
-    // script.js
-    document.getElementById('saveButton').addEventListener('click', function () {
+    const keywordsTextarea = document.getElementById('keywords');
+    const saveButton = document.getElementById('saveButton');
+
+    // Disable save button initially
+    saveButton.disabled = true;
+
+    // Add an event listener to the text area
+    keywordsTextarea.addEventListener('input', function () {
+        // Enable the save button if the text area has input, otherwise disable it
+        saveButton.disabled = keywordsTextarea.value.trim() === '';
+    });
+
+    saveButton.addEventListener('click', function () {
         // Replace this with your save functionality
         let k = $('#keywords').val();
 
         $.post('api/save_keyword.php', { k }, function (d) {
-            console.log(d)
+            getKeywords();
         })
     });
 
     // You should perform proper validation before sending data to the serve
-    $.get('api/view_keyword.php', function (d) {
-        console.log(d)
-    })
-    // fetch('api/view_keyword.php', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/x-www-form-urlencoded',
-    //     },
-    //     // body: username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)},
-    // })
-    //     .then(response => response.json())
-    //     .then(data => {
-    //             if (data.success) {
-    //                 console.log(data.keywords);
-    //                 data.keywords.forEach(keywords => {
-    //                     console.log(keywords); // Log each keyword to the console
-    //                 }); 
-    //             } else {
-    //                 errorMessage.textContent = 'Invalid username or password';
-    //             }
-    //     })
+    const getKeywords = function () {
+        $.get('api/view_keyword.php', function (d) {
+            let data = JSON.parse(d)
+            if (data.success) {
+                let rows = []
+                itr = 0;
+                data.keywords.forEach(keyword => {
+                    rows[itr++] = "<tr>"
+                    rows[itr++] = "<td>" + keyword.keywords + "</td>"
+                    rows[itr++] = "</tr>"
+                });
 
-    //     .catch(error => console.error('Error during login:', error));
+                $('table tbody#keywordTableBody').html(rows.join())
+            } else {
+                alert('No Data')
+            }
+        })
+    }
+    getKeywords();
 })
