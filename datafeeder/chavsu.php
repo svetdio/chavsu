e96817fa5d2598d6e270f809384f894bd69b2b27
svetdio <122599561+svetdio@users.noreply.github.com>
2023-12-10 01:14:51 +0800

    implement font awesome css; added action and response column

--- datafeeder/chavsu.php
+++ datafeeder/chavsu.php
@@ -6,8 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>CHAVSU</title>
   <!-- Link to external CSS file -->
+  <link rel="stylesheet" type="text/css" href="css/fontawesome.min.css">
   <link rel="stylesheet" type="text/css" href="css/styles.css">
-</head>
+  </head>
 
 <body>
 
@@ -29,10 +30,12 @@
   <!-- Table on the right side -->
   <div class="table-container">
     <table>
-      <caption>Entered Keywords</caption>
+      <caption>Entered Data</caption>
       <thead>
         <tr>
+          <th>Actions</th>
           <th>Keyword</th>
+          <th>Response</th>
         </tr>
       </thead>
       <tbody id="keywordTableBody">
