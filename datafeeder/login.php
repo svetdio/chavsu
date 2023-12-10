560bfd3ff772d23723be88123ecc7ca60c778ffc
svetdio <122599561+svetdio@users.noreply.github.com>
2023-12-10 20:12:57 +0800

    login

--- datafeeder/login.php
+++ datafeeder/login.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | CHAVSU Data Feeder</title>
     <link rel="stylesheet" type="text/css" href="css/fontawesome.min.css">
-    <link rel="stylesheet" href="css/styles1.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
     <div class="login-container">
@@ -17,11 +17,11 @@
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" required>
 
-            <button type="button" onclick="login()">Login</button>
+            <button type="button" onclick="login()">LOGIN</button>
         </form>
         <p id="errorMessage"></p>
     </div>
 
-    <script src="js/script1.js"></script>
+    <script src="js/login.js"></script>
 </body>
 </html>
