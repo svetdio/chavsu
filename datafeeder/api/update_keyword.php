99ac9e6ba79d48f57620b2953338e5178915eafa
svetdio <122599561+svetdio@users.noreply.github.com>
2023-12-12 14:08:34 +0800

    updated data feeder

--- datafeeder/api/update_keyword.php
+++ datafeeder/api/update_keyword.php
@@ -8,10 +8,12 @@ $conn = new mysqli($host, $user, $password, $database);
 
 $kid = $_POST['id'];
 $kword = $_POST['kword'];
+$resp = $_POST['resp'];
 
-$del_query = "UPDATE keywords SET
-keywords = '$kword'
-WHERE keyword_id = $kid";
+$del_query = "UPDATE keywords_response SET
+keywords = '$kword',
+response = '$resp'
+WHERE id = $kid";
 
 if (!$conn->query($del_query)) {
     echo json_encode(['error' => 'Error updating keywords']);
