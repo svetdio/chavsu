9cd9768d0e46176f90732e6b3b88e0a86f0b69d6
svetdio <122599561+svetdio@users.noreply.github.com>
2024-01-04 16:29:54 +0800

    frontend ui

---
+++ frontend/login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" href="css/vars.css" />
+    <link rel="stylesheet" href="css/login.css" />
+    <title>Login | CHAVSU</title>
+
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+
+<body>
+    <div class="log-in-dashboard">
+        <div class="web"></div>
+        <img class="chavsu-head" src="chavsu-head.png" />
+        <img class="help" src="help.png" />
+        <img class="bg" src="background.png" />
+        <div class="forgot-pass">Forgot password?</div>
+        <div class="forgot-pass2">
+            <span>
+                <span class="forgot-pass-2-span">Don’t have an account?</span>
+                <span class="forgot-pass-2-span2">Register here</span>
+            </span>
+        </div>
+        <div class="help2">Help</div>
+        <img class="log" src="log.png" />
+        <div class="log-in-yacc">Log-in with your CvSU account</div>
+
+        <div class="login-container">
+            <label for="email" class="cvsu-email">CvSU Email</label>
+            <input type="email" id="email" class="email" placeholder="Enter your email">
+            <label for="pass" class="password">Password</label>
+            <input type="pass" id="pass" class="pass" placeholder="Enter your password">
+        </div>
+
+        <button type="submit" class="log-in">Log in</button>
+
+
+        
+    </div>
+</body>
+
+</html>
