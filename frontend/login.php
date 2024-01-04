457a673acca3b8729a769c2bd53c649bdac25ced
svetdio <122599561+svetdio@users.noreply.github.com>
2024-01-04 19:45:30 +0800

    chavsu frontend

--- frontend/login.php
+++ frontend/login.php
@@ -2,46 +2,51 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Log in | CHAVSU</title>
+  <!-- Link to external CSS file -->
+  <link rel="stylesheet" type="text/css" href="css/login1.css">
 
-    <link rel="stylesheet" href="css/vars.css" />
-    <link rel="stylesheet" href="css/login.css" />
-    <title>Login | CHAVSU</title>
+  <!-- Link to external JavaScript file -->
 
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
 </head>
 
 <body>
-    <div class="log-in-dashboard">
-        <div class="web"></div>
-        <img class="chavsu-head" src="chavsu-head.png" />
-        <img class="help" src="help.png" />
-        <img class="bg" src="background.png" />
-        <div class="forgot-pass">Forgot password?</div>
-        <div class="forgot-pass2">
-            <span>
-                <span class="forgot-pass-2-span">Don’t have an account?</span>
-                <span class="forgot-pass-2-span2">Register here</span>
-            </span>
-        </div>
-        <div class="help2">Help</div>
-        <img class="log" src="log.png" />
-        <div class="log-in-yacc">Log-in with your CvSU account</div>
+<style>
+body  {
+  background-image: url("images/background.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  max-width: 100%;
+}
+</style>    
 
-        <div class="login-container">
-            <label for="email" class="cvsu-email">CvSU Email</label>
-            <input type="email" id="email" class="email" placeholder="Enter your email">
-            <label for="pass" class="password">Password</label>
-            <input type="pass" id="pass" class="pass" placeholder="Enter your password">
-        </div>
+    <img class="chavsu-head" src="images/chavsu-head.png" />
+    <img class="help" src="images/help.png" />
 
-        <button type="submit" class="log-in">Log in</button>
+    <!-- log in form -->
+    <div class="login-container">
+      <h2> <img class="log" src="images/log.png" /> Log-in with your CvSU account</h2>
+      <form id="loginForm">
+        <label for="email" class="cvsu-email">CvSU Email</label>
+        <input type="email" id="email" class="email" placeholder="Enter your email">
 
+        <label for="pass" class="password">Password</label>
+        <input type="password" id="pass" class="pass" placeholder="Enter your password">
 
-        
+        <div class="forgot-pass"><a href="#" style="color: black;">Forgot password?</a></div>
+
+        <button id="login" type="button"><strong>LOG IN</strong></button>
+      </form>
+      <p id="errorMessage"></p>
+
+      <div class="register">Don’t have an account? <strong><a href="#" style="color: black;">Register here</a></strong></div>
     </div>
+  </div>
 </body>
 
 </html>
