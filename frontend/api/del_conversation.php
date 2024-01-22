79c9138edfb95a09ff9306da02c4402d2093f78c
svetdio <122599561+svetdio@users.noreply.github.com>
2024-01-22 13:54:42 +0800

    modified delete convo

--- frontend/api/del_conversation.php
+++ frontend/api/del_conversation.php
@@ -8,15 +8,15 @@ $db = new mysqli($host, $user, $password, $database);
 
 $conv_id = $_POST['conv_id'];
 
-$query = "DELETE FROM conversation WHERE conv_id = '$conv_id'";
-
-if ($db->query($query)) {
-    $query2 = "DELETE FROM conversation_history WHERE conv_id = '$conv_id'";
-    if ($db->query($query2)) {
+$query2 = "DELETE FROM conversation_history WHERE conv_id = '$conv_id'";
+if ($db->query($query2)) {
+    // Now proceed to delete from the conversation table
+    $query = "DELETE FROM conversation WHERE conv_id = '$conv_id'";
+    if ($db->query($query)) {
         echo json_encode(['success' => true, 'message' => "Conversation successfully deleted"]);
     } else {
         echo json_encode(['success' => false, 'error' => 'Failed to delete conversation.']);
     }
 } else {
-    echo json_encode(['success' => false, 'error' => 'Failed to delete conversation.']);
+    echo json_encode(['success' => false, 'error' => 'Failed to delete conversation history.']);
 }
