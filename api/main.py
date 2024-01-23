0889d2cbcf65e0cc213db942a8400b4b89668e0a
Johnroe Paulo Cañamaque <Johnroe.Canamaque@websan.com>
2024-01-23 16:45:46 +0800

    Fetched faq from database

--- api/main.py
+++ api/main.py
@@ -1,8 +1,9 @@
 from typing import Union
+
 import os
 from dotenv import load_dotenv
 
-from llama_index import OpenAIEmbedding, VectorStoreIndex, SimpleDirectoryReader, PromptHelper, ServiceContext, StorageContext, load_index_from_storage
+from llama_index import OpenAIEmbedding, VectorStoreIndex, SimpleDirectoryReader, PromptHelper, ServiceContext, StorageContext, load_index_from_storage, download_loader
 from llama_index.llms import OpenAI
 from llama_index.text_splitter import SentenceSplitter
 from fastapi import FastAPI
@@ -32,28 +33,50 @@ def Chavsu(q):
 
     # Check if we have the persistent index installed
     # If true, create the index from scratch, else use existing index
-    if not os.path.exists(os.getenv('PERSIST_DIR')):
-        # load the documents and create the index
-        documents =  SimpleDirectoryReader(os.getenv('DATA_SOURCE')).load_data()
+    # if not os.path.exists(os.getenv('PERSIST_DIR')):
+    #     # load the documents and create the index
+    #     documents =  SimpleDirectoryReader(os.getenv('DATA_SOURCE')).load_data()
         
-        llm = OpenAI(model="gpt-3.5-turbo", temperature=0.3)
-        splitter = SentenceSplitter(chunk_size=256, chunk_overlap=20)
-        embed_model= OpenAIEmbedding()
+    #     llm = OpenAI(model="gpt-3.5-turbo", temperature=0.3)
+    #     splitter = SentenceSplitter(chunk_size=256, chunk_overlap=20)
+    #     embed_model= OpenAIEmbedding()
 
-        gpt_35_context = ServiceContext.from_defaults(
-            llm=llm,
-            text_splitter=splitter,
-            embed_model=embed_model
-        )
-        index = VectorStoreIndex.from_documents(documents, service_context=gpt_35_context)
+    #     gpt_35_context = ServiceContext.from_defaults(
+    #         llm=llm,
+    #         text_splitter=splitter,
+    #         embed_model=embed_model
+    #     )
+    #     index = VectorStoreIndex.from_documents(documents, service_context=gpt_35_context)
 
-        # store it for later
-        index.storage_context.persist(persist_dir=os.getenv('PERSIST_DIR'))
-    else:
-        # Load if 
-        # load the existing index
-        storage_context = StorageContext.from_defaults(persist_dir=os.getenv('PERSIST_DIR'))
-        index = load_index_from_storage(storage_context)
+    #     # store it for later
+    #     index.storage_context.persist(persist_dir=os.getenv('PERSIST_DIR'))
+    # else:
+    #     # load the existing index
+    #     storage_context = StorageContext.from_defaults(persist_dir=os.getenv('PERSIST_DIR'))
+    #     index = load_index_from_storage(storage_context)
+
+     # load the documents and create the index
+    
+    DatabaseReader = download_loader('DatabaseReader')
+
+    reader = DatabaseReader(
+        uri = os.getenv('DATA_SOURCE_URI')
+    )
+    query = f"""
+        SELECT response FROM keywords_response
+    """
+    documents = reader.load_data(query=query)
+    
+    llm = OpenAI(model="gpt-3.5-turbo", temperature=0.3)
+    splitter = SentenceSplitter(chunk_size=256, chunk_overlap=20)
+    embed_model= OpenAIEmbedding()
+
+    gpt_35_context = ServiceContext.from_defaults(
+        llm=llm,
+        text_splitter=splitter,
+        embed_model=embed_model
+    )
+    index = VectorStoreIndex.from_documents(documents, service_context=gpt_35_context)
 
     query_engine = index.as_chat_engine(chat_mode='context')
     response = query_engine.chat(q)
